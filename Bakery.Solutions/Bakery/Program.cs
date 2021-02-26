2c33915fc49dbe16e5e6357c364e3e99845dbb53
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-26 11:39:28 -0800

    Trying to make UI output more modular instead of one big block in Main method

--- Bakery.Solutions/Bakery/Program.cs
+++ Bakery.Solutions/Bakery/Program.cs
@@ -8,18 +8,36 @@ namespace Bakery
     {
         public static void Main()
         {
-            Loaves bread = new Loaves();
-            Pastries pastry = new Pastries();
+            Greeting();
+            GatherInputs();
+            
+        }
+
+        public void Greeting()
+        {
             Console.WriteLine("Hello! Welcome to Mikey's Bakery! We have loaves of bread and pastries for sale!");
             Console.WriteLine("Today we have some specials deals!");
             Console.WriteLine("Buy two loaves of bread and get one free! We also offer 3 pastries for $5");
             Console.WriteLine("Single Loaves are $5 each and single pastries are $2 each!");
+        }
+
+        public void GatherInputs()
+        {
             Console.WriteLine("How many loaves of bread would you like?");
             int loavesOfBread = int.Parse(Console.ReadLine());
-            bread.BreadCost(loavesOfBread);
             Console.WriteLine("How many pastries would you like?");
             int amountOfPastries = int.Parse(Console.ReadLine());
+            CallCostMethods(loavesOfBread, amountOfPastries);
+        }
+
+        public void CallCostMethods(int loavesOfBread, int amountOfPastries)
+        {
+            bread.BreadCost(loavesOfBread);
             pastry.PastryCost(amountOfPastries);
+        }
+
+        public void OutputPrice()
+        {
             Console.WriteLine("Your total will be: $" + (bread.BreadPrice + pastry.PastryPrice));
             Console.WriteLine("Thank you for your purchase!");
         }
