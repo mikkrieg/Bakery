cc46b803e3e2fd04984055ebdb49bcb2e0ecbaa6
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-26 08:38:30 -0800

    Create test classes and using directives in test file

---
+++ Bakery.Solutions/Bakery.Tests/Model.Tests/BakeryTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectName;
+using Bakery.Bread;
+using Bakery.Pastry;
+
+namespace Bakery.Tests
+{
+    [TestClass]
+    public class PastriesTests
+    {
+
+    }
+    
+    [TestClass]
+    public class LoavesTests
+    {
+
+    }
+}
