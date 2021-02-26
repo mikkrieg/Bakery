20e2aa9c989ea5ed721b3fd071fa15db15352784
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-26 10:45:07 -0800

    Add logic to PastryCost method, pass second test

--- Bakery.Solutions/Bakery/Models/Pastry.cs
+++ Bakery.Solutions/Bakery/Models/Pastry.cs
@@ -2,6 +2,20 @@ namespace Bakery.Pastry
 {
     public class Pastries
     {
-        
+        public int PastryPrice { get; set; }
+
+        public void PastryCost(int numberOfPastries)
+        {
+            if(numberOfPastries >= 3)
+            {
+                int numberOfBundles = (numberOfPastries / 3);
+                int numberOfSingles = (numberOfPastries % 3);
+                PastryPrice = (numberOfBundles * 5) + (numberOfSingles * 2);
+            }
+            else 
+            {
+                PastryPrice = (numberOfPastries * 2);
+            }
+        }
     }
 }
