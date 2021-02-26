d16dcc26cd77e09d4ff087bfef0a0c0d931f13ad
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-26 10:20:04 -0800

    Add some Console.WriteLines and create variables with user input

--- Bakery.Solutions/Bakery/Program.cs
+++ Bakery.Solutions/Bakery/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using Bakery.Bread;
 using Bakery.Pastry;
 
@@ -7,7 +8,17 @@ namespace Bakery
     {
         public static void Main()
         {
-            
+            Loaves bread = new Loaves();
+            Pastries pastry = new Pastries();
+            Console.WriteLine("Hello! Welcome to Mikey's Bakery! We have loaves of bread and pastries for sale!");
+            Console.WriteLine("Today we have some specials deals!");
+            Console.WriteLine("Buy two loaves of bread and get one free! We also offer 3 pastries for $5");
+            Console.WriteLine("Loaves are $5 each and pastries are $2 each!");
+            Console.WriteLine("How many loaves of bread would you like?");
+            int loavesOfBread = int.Parse(Console.ReadLine());
+            Console.WriteLine("How many pastries would you like?");
+            int amountOfPastries = int.Parse(Console.ReadLine());
+
         }
     }
 }
