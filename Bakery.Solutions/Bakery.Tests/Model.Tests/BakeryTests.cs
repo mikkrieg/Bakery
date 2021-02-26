e61920efaed54a89facfb8cf405aedbb099082a2
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-26 09:05:51 -0800

    Hook up first test get a good fail

--- Bakery.Solutions/Bakery.Tests/Model.Tests/BakeryTests.cs
+++ Bakery.Solutions/Bakery.Tests/Model.Tests/BakeryTests.cs
@@ -1,18 +1,26 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProjectName;
+using Bakery;
 using Bakery.Bread;
 using Bakery.Pastry;
 
 namespace Bakery.Tests
 {
     [TestClass]
-    public class PastriesTests
-    {
-
-    }
-    
-    [TestClass]
     public class LoavesTests
+    {
+        [TestMethod]
+        public void BreadCost_SetsPriceOfBread_Int()
+        {
+            int bread = 5;
+            int shouldCost = 20;
+            Loaves boughtBread = new Loaves();
+            int result = boughtBread.BreadCost(bread);
+            Assert.AreEqual(shouldCost, result);
+        }
+    }
+
+    [TestClass]
+    public class PastriesTests
     {
 
     }
