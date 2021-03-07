079898c44c2db3397bec374e419770a54d7da93c
Mikey Kriegel <mikkrieg@gmail.com>
2021-03-06 18:31:53 -0800

    Create more tests to test methods thouroughly

--- Bakery.Solutions/Bakery.Tests/Model.Tests/BakeryTests.cs
+++ Bakery.Solutions/Bakery.Tests/Model.Tests/BakeryTests.cs
@@ -7,10 +7,27 @@ namespace Bakery.Tests
     public class BreadTests
     {
         [TestMethod]
+        public void Constructor_CreatesInstanceOfBread_Bread()
+        {
+            Bread newBread = new Bread();
+            Assert.AreEqual(typeof(Bread), newBread.GetType());
+        }
+
+        [TestMethod]
+        public void BreadCost_SetsPriceOfBreadWithDiscount_Int()
+        {
+            int breadAmount = 3;
+            int shouldCost = 10;
+            Bread boughtBread = new Bread();
+            boughtBread.BreadCost(breadAmount);
+            Assert.AreEqual(shouldCost, boughtBread.BreadPrice);
+        }
+
+        [TestMethod]
         public void BreadCost_SetsPriceOfBread_Int()
         {
-            int breadAmount = 5;
-            int shouldCost = 20;
+            int breadAmount = 1;
+            int shouldCost = 5;
             Bread boughtBread = new Bread();
             boughtBread.BreadCost(breadAmount);
             Assert.AreEqual(shouldCost, boughtBread.BreadPrice);
@@ -21,10 +38,27 @@ namespace Bakery.Tests
     public class PastryTests
     {
         [TestMethod]
-        public void PastryCost_SetsPriceOfBread_Int()
+        public void Constructor_CreatesInstanceOfPastry_Pastry()
+        {
+            Pastry newPastry = new Pastry();
+            Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+        }
+
+        [TestMethod]
+        public void PastryCost_SetsPriceOfPastryWithDiscount_Int()
         {
             int pastryAmount = 5;
             int shouldCost = 9;
+            Pastry boughtPastry = new Pastry();
+            boughtPastry.PastryCost(pastryAmount);
+            Assert.AreEqual(shouldCost, boughtPastry.PastryPrice);
+        }
+
+        [TestMethod]
+        public void PastryCost_SetsPriceOfPastry_Int()
+        {
+            int pastryAmount = 1;
+            int shouldCost = 2;
             Pastry boughtPastry = new Pastry();
             boughtPastry.PastryCost(pastryAmount);
             Assert.AreEqual(shouldCost, boughtPastry.PastryPrice);
@@ -35,10 +69,27 @@ namespace Bakery.Tests
     public class DonutTests
     {
         [TestMethod]
-        public void DonutCost_SetsPriceOFDonut_Int()
+        public void Constructor_CreatesInstanceOfDonut_Donut()
+        {
+            Donut newDonut = new Donut();
+            Assert.AreEqual(typeof(Donut), newDonut.GetType());
+        }
+
+        [TestMethod]
+        public void DonutCost_SetsPriceOfDonutWithDiscount_Int()
         {
             int donutAmount = 12;
             int shouldCost = 10;
+            Donut boughtDonuts = new Donut();
+            boughtDonuts.DonutCost(donutAmount);
+            Assert.AreEqual(shouldCost, boughtDonuts.DonutPrice);
+        }
+
+        [TestMethod]
+        public void DonutCost_SetsPriceOfDonut_Int()
+        {
+            int donutAmount = 1;
+            int shouldCost = 2;
             Donut boughtDonuts = new Donut();
             boughtDonuts.DonutCost(donutAmount);
             Assert.AreEqual(shouldCost, boughtDonuts.DonutPrice);
